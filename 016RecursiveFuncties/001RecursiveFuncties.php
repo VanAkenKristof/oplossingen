97bb71bf7cb7f81e125fd56e651d2d369fc159b6
vanakenkristof <vanakenk@gmail.com>
2014-11-11 15:41:28 +0100

    Aanpassen recursive en afmaken Get

--- 016RecursiveFuncties/001RecursiveFuncties.php
+++ 016RecursiveFuncties/001RecursiveFuncties.php
@@ -1,17 +1,61 @@
 <?php 
-$berekenGeld = 100000;
-$intrest = 0;
+// $berekenGeld = 100000;
+// $intrest = 0;
 
-//Is niet gelukt met een recursive function
-for ($i=1; $i < 11; $i++) { 
-	$intrest = $berekenGeld * 0.08;
-	$berekenGeld += $intrest;
-	echo "In jaar ".$i." was er ".$berekenGeld." in totaal en was de intrest ".$intrest."<br>";
+// for ($i=1; $i < 11; $i++) { 
+// 	$intrest = $berekenGeld * 0.08;
+// 	$berekenGeld += $intrest;
+// 	echo "In jaar ".$i." was er ".$berekenGeld." in totaal en was de intrest ".$intrest."<br>";
 
 
+// 	}
+
+
+	$startKapitaal 	=	100000;
+	$renteVoet		=	8;
+	$aantalJaar		=	10;
+
+	$bericht = "";
+
+	function berekenGeld($startKapitaal, $renteVoet, $aantalJaar){
+		static $i = 1;
+		static $arrayDump 	= 	array();
+		
+		$winst 	= $startKapitaal * ( $renteVoet / 100);
+		$totaal	=	$startKapitaal + $winst;
+
+		$arrayDump[] =  "In jaar ".$i." was er ".$totaal." in totaal en was de intrest ".$winst."<br>";
+		
+
+		if ($i < $aantalJaar)
+		{
+			$i++;
+			berekenGeld( $totaal, $renteVoet, $aantalJaar );
+		}
+
+		return $arrayDump;
 	}
 
-
-
+	$winstHans = berekenGeld( $startKapitaal, $renteVoet, $aantalJaar );
 
  ?>
+
+ <!doctype html>
+ <html>
+     <head>
+         <meta charset="utf-8">
+         <meta name="description" content="">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <title>Untitled</title>
+         <link rel="stylesheet" href="css/style.css">
+         <link rel="author" href="humans.txt">
+     </head>
+     <body>
+         <ul>
+			<?php foreach($winstHans as $value): ?>
+				<li><?php echo $value ?></li>
+			<?php endforeach ?>
+		</ul>
+         <script src="js/main.js"></script>
+     </body>
+ </html>
