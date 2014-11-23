84edc9eb313e275ec70f407bdfd52f9a6549d46d
vanakenkristof <vanakenk@gmail.com>
2014-11-23 23:31:35 +0100

    Periode oefening afgewerkt

--- periodeopdracht-01-todo/index.php
+++ periodeopdracht-01-todo/index.php
@@ -1,13 +1,50 @@
 <?php
 session_start();
 
-$firstpost = true;
-$message1 = "Je hebt nog geen TODO's toegevoegd. Zo weinig werk of meesterplanner?";
-
-if ( isset ( $_POST[ 'submit' ] ) ) {
-	$_SESSION['todo'][] = $_POST['todoitem'];
+function __autoload($class_name) {
+	require_once 'classes/'. $class_name . '.php';
 }
 
+if (isset($_POST['done'])){
+	$item = $_POST['itemInList'];
+	$_SESSION['todo'][(int)$item]->setCompletedTrue();
+}
+
+
+if (isset($_POST['delete'])){
+	$item = $_POST['itemInList'];
+	unset($_SESSION['todo'][(int)$item]);
+	$_SESSION['todo'] = array_values($_SESSION['todo']);
+}
+
+
+if ( isset ( $_POST[ 'submit' ] ) ) {
+	if (empty($_POST['todoitem'])){
+		echo "Invul kader is leeg, gelieven iets in te vullen";
+	}
+	else{
+
+		$_SESSION['todo'][] = new ToDo($_POST['todoitem']);
+	}
+}
+
+function doChecks1() {
+	foreach($_SESSION['todo'] as $id => $todo){
+		if ($todo->getItemCompleted()){
+			return false;
+		}
+	}
+	return true;
+}
+
+function doChecks2() {
+	foreach($_SESSION['todo'] as $id => $todo){
+		if (!$todo->getItemCompleted()){
+			return false;
+		}
+	}
+	return true;
+}
 
 ?>
 
@@ -17,11 +54,12 @@ if ( isset ( $_POST[ 'submit' ] ) ) {
 	<meta charset="utf-8">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Untitled</title>
+	<title>ToDo Lijst</title>
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="author" href="humans.txt">
 	<style>
-
+	#doneKnop {border-radius: 20px; background-color: green; margin-right: 10px;}
+	#deleteKnop {border-radius: 20px; background-color: red; margin-left: 10px;}
 
 
 
@@ -30,9 +68,20 @@ if ( isset ( $_POST[ 'submit' ] ) ) {
 </head>
 <body>
 	<h1>Todo App</h1>
-	<?php if (isset($_SESSION['todo'])): ?>
+	<?php if (isset($_SESSION['todo']) && !empty($_SESSION['todo'])): ?>
+	<?php echo "<h2>Nog te doen</h2>".(doChecks2() ? "<p>Schouderklopje, alles is gedaan!</p>":"")?>
 	<?php foreach($_SESSION['todo'] as $id => $todo): ?>
-		<?php echo $todo."<br>"?>
+	<?php if (!$todo->getItemCompleted()):?>
+	<?php echo "<form action=\"index.php\" method=\"POST\"><input type=\"hidden\" name=\"itemInList\" value=\"".$id."\"><p><input type=\"submit\" name=\"done\" value=\"V\" id=\"doneKnop\">"
+	.$todo->getName()."<input type=\"submit\" name=\"delete\" value=\"X\" id=\"deleteKnop\"></p></form>"?>
+<?php endif;?>
+<?php endforeach; ?>
+<?php echo "<h2>Done and done!</h2>".(doChecks1() ? "<p>Werk aan de winkel...</p>":"")?>
+<?php foreach($_SESSION['todo'] as $id => $todo): ?>
+	<?php if ($todo->getItemCompleted()):?>
+	<?php echo "<form action=\"index.php\" method=\"POST\"><input type=\"hidden\" name=\"itemInList\" value=\"".$id."\"><p><strike><input type=\"submit\" name=\"done\" value=\"V\" id=\"doneKnop\">"
+	.$todo->getName()."<input type=\"submit\" name=\"delete\" value=\"X\" id=\"deleteKnop\"></strike></p></form>"?>
+<?php endif;?>
 <?php endforeach; ?>
 <?php else :?>
 	<?php echo "Je hebt nog geen TODO's toegevoegd. Zo weinig werk of meesterplanner?"?>
